694d24abd188bce405140c86fbbaf81844a53855
Sebastian Bodo Hennicke <sebastian.hennicke@brightstraining.com>
2023-02-01 15:43:20 +0100

    NewsAgency added

---
+++ src/de/brightslearning/java/advanced/designpattern/observer/NewsAgency.java
@@ -0,0 +1,29 @@
+package de.brightslearning.java.advanced.designpattern.observer;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class NewsAgency implements Channel{
+    private String news;
+    private List<Channel> channels = new ArrayList<>();
+
+    public void addObserver(Channel channel) {
+        this.channels.add(channel);
+    }
+
+    public void removeObserver(Channel channel) {
+        this.channels.remove(channel);
+    }
+
+    public void setNews(String news) {
+        this.news = news;
+        for (Channel channel : this.channels) {
+            channel.update(this.news);
+        }
+    }
+
+    @Override
+    public void update(Object news) {
+        this.setNews((String) news);
+    }
+}
